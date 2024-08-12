521b62bae172e898da3ab4ab047b548edb540780
alloen9425 <allone9425@gmail.com>
2024-08-12 13:42:18 +0900

    feat: 기본 버튼 컴포넌트 개발 (#4)

--- src/app/(page)/dw/page.tsx
+++ src/app/(page)/dw/page.tsx
@@ -1,7 +1,10 @@
+import Button from "@/components/Button/Button";
+
 const dw = () => {
   return (
     <>
       <h1>dw Component</h1>
+      <Button>기본 버튼</Button>
     </>
   );
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,12 @@
-const Button = () => {
-  return (
-    <>
-      <h1>Button Component</h1>
-    </>
-  );
+type ButtonProps = {
+  children: React.ReactNode;
 };
+
+const Button: React.FC<ButtonProps> = ({ children }) => {
+  const basicButton =
+    "text-white font-bold text-sm rounded bg-[#9AC5E5] hover:bg-[#EDCE7A] px-3 py-2 ";
+
+  return <button className={`${basicButton}`}>{children}</button>;
+};
+
 export default Button;
