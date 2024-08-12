e03a2a3f7c37b97cab45c90c605db64bb40725e4
alloen9425 <allone9425@gmail.com>
2024-08-12 14:47:11 +0900

    feat: 버튼 컴포넌트의 버튼 사이즈 추가 (#4)

--- src/app/(page)/dw/page.tsx
+++ src/app/(page)/dw/page.tsx
@@ -4,7 +4,12 @@ const dw = () => {
   return (
     <>
       <h1>dw Component</h1>
-      <Button>기본 버튼</Button>
+      <div className="space-x-4">
+        <Button>기본 버튼</Button>
+        <Button size="small">작은 버튼</Button>
+        <Button size="medium">중간 버튼</Button>
+        <Button size="large">큰 버튼</Button>
+      </div>
     </>
   );
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,24 @@
 type ButtonProps = {
+  size?: "small" | "medium" | "large";
   children: React.ReactNode;
 };
 
-const Button: React.FC<ButtonProps> = ({ children }) => {
+const Button: React.FC<ButtonProps> = ({
+  size = "medium",
+
+  children,
+}) => {
+  const buttonSize =
+    size === "small"
+      ? "px-2 py-2 text-xs"
+      : size === "medium"
+        ? "px-3 py-2 text-sm"
+        : "px-5 py-2 text-lg";
+
   const basicButton =
     "text-white font-bold text-sm rounded bg-[#9AC5E5] hover:bg-[#EDCE7A] px-3 py-2 ";
 
-  return <button className={`${basicButton}`}>{children}</button>;
+  return <button className={`${basicButton} ${buttonSize}`}>{children}</button>;
 };
 
 export default Button;
