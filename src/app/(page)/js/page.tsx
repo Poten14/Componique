0859b2435b4398d6c33b6bca270974e80db2b6a8
김준수 <junsu462@naver.com>
2024-08-12 23:13:28 +0900

    feat: Card1 clear

--- src/app/(page)/js/page.tsx
+++ src/app/(page)/js/page.tsx
@@ -1,8 +1,13 @@
-const js = () => {
+import Card1 from "@/components/Card/Card1";
+
+const page = () => {
   return (
-    <>
-      <h1>js Component</h1>
-    </>
+    <div>
+      <Card1
+        title="Next.js"
+        description="Next.js is a React framework for building full-stack web applications. You use React Components to build user interfaces, and Next.js for additional features and optimizations."
+      />
+    </div>
   );
 };
-export default js;
+export default page;
