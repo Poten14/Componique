84e65eec0090d95dd09ac2bac35c630094889693
alloen9425 <allone9425@gmail.com>
2024-08-12 16:25:17 +0900

    feat: 버튼 컴포넌트의 버튼 컬러 추가 (#4)

--- src/app/(page)/dw/page.tsx
+++ src/app/(page)/dw/page.tsx
@@ -10,6 +10,23 @@ const dw = () => {
         <Button size="medium">중간 버튼</Button>
         <Button size="large">큰 버튼</Button>
       </div>
+      <div className="space-x-4 space-y-4">
+        <Button color="primary">Primary 버튼</Button>
+        <Button color="secondary">Secondary 버튼</Button>
+        <Button color="success">Success 버튼</Button>
+        <Button color="warning">Warning 버튼</Button>
+        <Button color="danger">Danger 버튼</Button>
+      </div>
+      <div className="space-x-4 space-y-4">
+        <Button color="red">Red 버튼</Button>
+        <Button color="orange">Orange 버튼</Button>
+        <Button color="yellow">Yellow 버튼</Button>
+        <Button color="green">Green 버튼</Button>
+        <Button color="blue">Blue 버튼</Button>
+        <Button color="purple">Purple 버튼</Button>
+        <Button color="pink">Pink 버튼</Button>
+        <Button color="basic">Basic 버튼</Button>
+      </div>
     </>
   );
 };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,25 @@
 type ButtonProps = {
   size?: "small" | "medium" | "large";
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "warning"
+    | "danger"
+    | "red"
+    | "orange"
+    | "yellow"
+    | "green"
+    | "blue"
+    | "purple"
+    | "pink"
+    | "basic";
   children: React.ReactNode;
 };
 
 const Button: React.FC<ButtonProps> = ({
   size = "medium",
-
+  color = "basic",
   children,
 }) => {
   const buttonSize =
@@ -15,10 +29,31 @@ const Button: React.FC<ButtonProps> = ({
         ? "px-3 py-2 text-sm"
         : "px-5 py-2 text-lg";
 
+  const buttonColor: { [key in NonNullable<ButtonProps["color"]>]: string } = {
+    //Key in을 쓴 이유 : buttonColor의 모든 키가 ButtonProps의 color에 정의한 값 중 하나여야 해서 사용함 => 타입스크립트에서 자동완성이 가능함
+    primary: "bg-[#7AA7FF] hover:bg-[#4C89FF]",
+    secondary: "bg-[#C294F0] hover:bg-[#A662E9]",
+    success: "bg-[#7EEFAF] hover:bg-[#33E680]",
+    warning: "bg-[#EDCE7B] hover:bg-[#E7BC4B]",
+    danger: "bg-[#FF7676] hover:bg-[#FF4C4C]",
+    red: "bg-red-500 hover:bg-red-700",
+    orange: "bg-orange-500 hover:bg-orange-700",
+    yellow: "bg-yellow-500 hover:bg-yellow-700",
+    green: "bg-green-500 hover:bg-green-700",
+    blue: "bg-blue-500 hover:bg-blue-700",
+    purple: "bg-purple-500 hover:bg-purple-700",
+    pink: "bg-pink-500 hover:bg-pink-700",
+    basic: "bg-[#9AC5E5] hover:bg-[#71ADDA]",
+  };
+
   const basicButton =
     "text-white font-bold text-sm rounded bg-[#9AC5E5] hover:bg-[#EDCE7A] px-3 py-2 ";
 
-  return <button className={`${basicButton} ${buttonSize}`}>{children}</button>;
+  return (
+    <button className={`${basicButton} ${buttonSize} ${buttonColor[color]}`}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
