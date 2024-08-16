ad0e48daa5a4ecb37731e8a6c4fdd31462d1730c
alloen9425 <allone9425@gmail.com>
2024-08-16 23:23:14 +0900

    feat: Toast 컴포넌트의 포지션 추가와 사이즈 수정 (#76)

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -7,7 +7,17 @@ import { Color16, Size } from "types/type";
 type ToastProps = {
   size?: Size;
   isOpen?: boolean;
-  postion?: "top" | "bottom" | "left" | "right";
+  isClose?: boolean;
+  position?:
+    | "leftTop"
+    | "leftBottom"
+    | "rightTop"
+    | "rightBottom"
+    | "centerTop"
+    | "centerBottom"
+    | "left"
+    | "right"
+    | "center";
   onClose?: () => void;
   children: React.ReactNode;
   color?: Color16;
@@ -19,6 +29,7 @@ const Toast: React.FC<ToastProps> = ({
   onClose,
   color = "basic",
   size = "large",
+  position = "centerBottom",
   ...rest
 }) => {
   const [isToastOpen, setIsToastOpen] = useState(isOpen);
@@ -32,9 +43,14 @@ const Toast: React.FC<ToastProps> = ({
     setIsToastOpen(false);
     if (onClose) onClose();
   };
-
-  const ToastSize =
-    size === "small" ? "w-4/12" : size === "medium" ? "w-6/12" : "w-full";
+  const ToastSize = {
+    small:
+      "min-w-[200px] sm:min-w-[250px]  md:min-w-[390px] lg:min-w-[575px] xl:min-w-[650px]",
+    medium:
+      "min-w-[300px] sm:min-w-[375px]  md:min-w-[585px] lg:min-w-[863px] xl:min-w-[975px]",
+    large:
+      "min-w-[400px] sm:min-w-[500px]  md:min-w-[780px] lg:min-w-[1150px] xl:min-w-[1300px]",
+  };
 
   const bgColors = {
     primary: "bg-Primary",
@@ -54,24 +70,34 @@ const Toast: React.FC<ToastProps> = ({
     gray: "bg-gray",
     black: "bg-black",
   };
+  const ToastPosition = {
+    leftTop: "top-0 left-0",
+    centerTop: "top-0 left-1/2 -translate-x-1/2 ",
+    rightTop: "top-0  right-0 ",
+    left: "top-1/2 left-0 transform -translate-y-1/2 ",
+    center: "top-1/2 left-1/2 -translate-x-1/2 ",
+    right: "top-1/2 right-0 ",
+    leftBottom: "bottom-0 left-0 ",
+    centerBottom: "bottom-0 left-1/2 -translate-x-1/2 ",
+    rightBottom: "bottom-0 right-0  ",
+  };
 
-  const BasicToast =
-    "min-w-md fixed left-1/2 top-10 w-11/12 -translate-x-1/2 -translate-y-1/2 transform";
+  const BasicToast = "min-w-md   box-border fixed select-none ";
   return (
-    <>
+    <div>
       <section
-        className={`${BasicToast} ${
+        className={`${BasicToast} ${ToastPosition[position]} ${ToastSize[size]} ${
           isToastOpen ? "opacity-100" : "pointer-events-none opacity-0"
         }`}
       >
         <div
-          className={`flex w-full items-center justify-between rounded-md bg-Basic ${bgColors[color]} p-4 ${ToastSize}`}
+          className={`flex items-center justify-between rounded-md bg-Basic ${bgColors[color]} p-4`}
           {...rest}
         >
           {children}
 
           <button
-            className="group relative h-4 w-4 bg-transparent"
+            className="group relative ml-2 h-4 w-4 bg-transparent"
             onClick={onclickCloseHandler}
           >
             <span className="absolute left-1/2 top-1/2 block h-0.5 w-full -translate-x-1/2 -translate-y-1/2 rotate-45 transform bg-white"></span>
@@ -79,7 +105,7 @@ const Toast: React.FC<ToastProps> = ({
           </button>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 export default Toast;
