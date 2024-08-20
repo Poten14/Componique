1a6e35fa4b0d00235fcf807e8666a85079172792
choyoungrae <whdudfo0124@gmail.com>
2024-08-20 18:35:50 +0900

    feat : 첫날, 마지막날, 이전다음페이지 기능 구현(#91)

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,16 +1,61 @@
 "use client";
 
-interface CalendarProps {}
+import { useState } from "react";
 
-const Calendar = () => {
-  const weeks = ["일", "월", "화", "수", "목", "금", "토"];
+interface CalendarProps {
+  selectedDate?: Date;
+  onDateSelect?: (date: Date) => void;
+}
+
+const Calendar = ({ selectedDate, onDateSelect }: CalendarProps) => {
+  const [currentDate, setCurrentDate] = useState<Date>(new Date());
+  const year = currentDate.getFullYear();
+  const month = currentDate.getMonth();
+
+  // 현재 달의 첫 날
+  // 현재 달의 첫 날짜 요일을 숫자로 반환 일 = 1, 월 = 1
+  const firstDayOfMonth = new Date(year, month, 1);
+  // 달력 시작 날짜를 현재 달의 첫 날의 주의 일요일로 설정
+  const startDay = new Date(firstDayOfMonth);
+  startDay.setDate(1 - firstDayOfMonth.getDay());
+
+  // 현재 달의 마지막 날
+  const lastDayOfMonth = new Date(year, month + 1, 0);
+  // 달력 끝 날짜를 현재 달의 마지막 날의 주의 토요일로 설정
+  const endDay = new Date(lastDayOfMonth);
+  endDay.setDate(lastDayOfMonth.getDate() + (6 - lastDayOfMonth.getDay()));
+
+  const handlePrevMonth = () => {
+    setCurrentDate(
+      new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1),
+    );
+  };
+
+  const handleNextMonth = () => {
+    setCurrentDate(
+      new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1),
+    );
+  };
+
+  // 해당 날짜 선택
+  const handleDateClick = (day: number) => {
+    const selectedDate = new Date(
+      currentDate.getFullYear(),
+      currentDate.getMonth(),
+      day,
+    );
+    if (onDateSelect) {
+      onDateSelect(selectedDate);
+    }
+  };
 
   return (
     <>
       <div className="w-64">
         <div className="mb-2 flex justify-between">
-          <button>Prev</button>
-          <button>Next</button>
+          <button onClick={handlePrevMonth}>Prev</button>
+          <span>년</span>
+          <button onClick={handleNextMonth}>Next</button>
         </div>
         {/* 월,달,날짜 */}
         <div className="grid grid-cols-7 gap-1">
