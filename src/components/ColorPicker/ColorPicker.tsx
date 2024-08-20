134f6f50a580a219d2bc2f129e22210abb7a5c4a
alloen9425 <allone9425@gmail.com>
2024-08-20 15:13:14 +0900

    feat: ColorPicker 컴포넌트의 복사 기능 추가 (#96)

--- src/components/ColorPicker/ColorPicker.tsx
+++ src/components/ColorPicker/ColorPicker.tsx
@@ -1,18 +1,39 @@
 "use client";
 import { useState } from "react";
 import { ColorPickerProps } from "./ColorPickerType";
+import Button from "@components/Button/Button";
 const ColorPicker: React.FC<ColorPickerProps> = ({ colors }) => {
   const [selectColor, setSelectedColor] = useState<string>("#ffffff");
+  const [msg, setMsg] = useState("");
+  const onClickCopyHandler = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      setMsg("선택한 컬러가 복사되었습니다.");
+      setTimeout(() => setMsg(""), 2000);
+    } catch (error) {
+      setMsg("복사 실패하였습니다.");
+    }
+  };
   return (
     <>
-      <h1>ColorPicker Component</h1>
-      <div className="w-72 space-y-4 rounded-lg bg-slate-100 py-9 text-center">
-        <input
-          type="text"
-          value={selectColor}
-          className="w-24 p-2 text-center text-xl outline-none"
-          readOnly
-        />
+      <div className="m-5 w-72 space-y-4 rounded-lg bg-slate-100 py-9 text-center">
+        <div className="flex items-center justify-center">
+          <input
+            type="text"
+            value={selectColor}
+            className="mr-2 w-24 px-2 py-1 text-center text-xl outline-none"
+            readOnly
+          />
+          <Button
+            radius="small"
+            onClick={() => {
+              onClickCopyHandler(selectColor);
+            }}
+          >
+            Copy
+          </Button>
+        </div>
+        <p className="text-sm">{msg}</p>
         <div>
           <input
             type="color"
@@ -28,7 +49,7 @@ const ColorPicker: React.FC<ColorPickerProps> = ({ colors }) => {
             {colors.map((item, index) => (
               <div
                 key={index}
-                className={`h-[33px] w-[33px] rounded border border-slate-200 ${selectColor === item ? "border-2 border-slate-400" : ""}`}
+                className={`h-[33px] w-[33px] rounded border border-slate-200 ${selectColor === item ? "border-2 border-slate-500" : ""}`}
                 style={{ backgroundColor: item }}
                 onClick={() => {
                   console.log(item);
