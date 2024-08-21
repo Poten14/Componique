4e1c9f9d05017e0b3d4c0983dc65c9968fa835e6
alloen9425 <allone9425@gmail.com>
2024-08-21 14:56:19 +0900

    feat: ColorPicker 컴포넌트의 GradientPicker 구현 (#105)

--- src/app/(page)/dw2/page.tsx
+++ src/app/(page)/dw2/page.tsx
@@ -1,11 +1,14 @@
 "use Client";
 
 import ColorPickerUI from "@components/ColorPicker/ColorPickerUI";
+import GradientPicker from "@components/ColorPicker/GradientPicker";
 
 const dw2 = () => {
   return (
     <>
       <ColorPickerUI />
+
+      <GradientPicker />
     </>
   );
 };
