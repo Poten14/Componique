078180b2c7260ce6acb0fceecbafa1e55ff138a3
임성경 <5622lsk@naver.com>
2024-08-26 11:49:26 +0900

    mdx세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+import withMDX from "@next/mdx";
 
-export default nextConfig;
+const nextConfig = {
+  // 기타 Next.js 설정
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"], // mdx 확장자를 페이지로 인식
+};
+
+export default withMDX({
+  extension: /\.mdx?$/,
+})(nextConfig);
