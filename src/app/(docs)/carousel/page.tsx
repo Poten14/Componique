d8ce89cd82d357683ba4b006497d8d27b873c843
choyoungrae <whdudfo0124@gmail.com>
2024-08-29 00:38:17 +0900

    feat : docs 페이지번호매김구현(#207)

--- src/app/(docs)/carousel/page.tsx
+++ src/app/(docs)/carousel/page.tsx
@@ -16,14 +16,14 @@ const CarouselDocs: React.FC = () => {
 
   return (
     <div className="prose max-w-[1000px] p-5 text-[#6D6D6D]">
-      <h1 className="text-[#2D3748]">Carousel</h1>
+      <h1 className="text-[#2D3748]">1. Carousel</h1>
       <p>
         <code>Carousel</code> 컴포넌트는 여러 이미지를 슬라이드 방식으로
         보여주는 기능을 제공합니다. 사용자는 이전, 다음 버튼을 사용해 이미지를
         전환할 수 있습니다.
       </p>
 
-      <h2 className="text-[#2D3748]">Import</h2>
+      <h2 className="text-[#2D3748]">1.1. Import</h2>
       <CodeBox
         code={`import Carousel from '@components/Carousel';`}
         copyText={`import Carousel from '@components/Carousel';`}
@@ -33,7 +33,7 @@ const CarouselDocs: React.FC = () => {
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Usage</h2>
+      <h2 className="text-[#2D3748]">1.2. Usage</h2>
       <p>기본 사용 예제는 아래와 같습니다:</p>
       <div style={{ marginBottom: "20px" }}>
         <Carousel
@@ -65,7 +65,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Customization</h2>
+      <h2 className="text-[#2D3748]">1.3. Customization</h2>
       <p>
         <code>Carousel</code> 컴포넌트는 다양한 이미지 배열을 제공하며, 필요에
         따라 슬라이드 전환을 위한 추가 설정을 쉽게 변경할 수 있습니다.
@@ -111,7 +111,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Props</h2>
+      <h2 className="text-[#2D3748]">1.4. Props</h2>
       <p>
         <code>Carousel</code> 컴포넌트는 아래와 같은 Props를 가집니다:
       </p>
@@ -140,7 +140,7 @@ export default Example;`}
         </tbody>
       </table>
 
-      <h2 className="text-[#2D3748]">Full Example</h2>
+      <h2 className="text-[#2D3748]">1.5. Full Example</h2>
       <CodeBox
         code={`import Carousel from '@components/Carousel';
 
@@ -175,14 +175,14 @@ export default Example;`}
       />
       <br />
       <br />
-      <h1 className="text-[#2D3748]">CarouselAutoplay</h1>
+      <h1 className="text-[#2D3748]">2. CarouselAutoplay</h1>
       <p>
         <code>CarouselAutoplay</code> 컴포넌트는 여러 이미지를 슬라이드 방식으로
         자동 재생 기능과 함께 보여줍니다. 사용자는 이전, 다음 버튼을 사용해
         이미지를 수동으로 전환할 수 있으며, 자동 재생 기능도 포함되어 있습니다.
       </p>
 
-      <h2 className="text-[#2D3748]">Import</h2>
+      <h2 className="text-[#2D3748]">2.1. Import</h2>
       <CodeBox
         code={`import CarouselAutoplay from '@components/CarouselAutoplay';`}
         copyText={`import CarouselAutoplay from '@components/CarouselAutoplay';`}
@@ -192,7 +192,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Usage</h2>
+      <h2 className="text-[#2D3748]">2.2. Usage</h2>
       <p>기본 사용 예제는 아래와 같습니다:</p>
       <div style={{ marginBottom: "20px" }}>
         <CarouselAutoplay
@@ -232,7 +232,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Customization</h2>
+      <h2 className="text-[#2D3748]">2.3. Customization</h2>
       <p>
         <code>CarouselAutoplay</code> 컴포넌트는 자동 재생 기능과 슬라이드 전환
         간격을 조정할 수 있습니다. 사용자는 <code>autoplay</code>와{" "}
@@ -285,7 +285,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Props</h2>
+      <h2 className="text-[#2D3748]">2.4. Props</h2>
       <p>
         <code>CarouselAutoplay</code> 컴포넌트는 아래와 같은 Props를 가집니다:
       </p>
@@ -338,7 +338,7 @@ export default Example;`}
         </tbody>
       </table>
 
-      <h2 className="text-[#2D3748]">Full Example</h2>
+      <h2 className="text-[#2D3748]">2.5. Full Example</h2>
       <CodeBox
         code={`import CarouselAutoplay from '@components/CarouselAutoplay';
 
@@ -377,7 +377,7 @@ export default Example;`}
       />
       <br />
       <br />
-      <h1 className="text-[#2D3748]">CarouselDots</h1>
+      <h1 className="text-[#2D3748]">3. CarouselDots</h1>
       <p>
         <code>CarouselDots</code> 컴포넌트는 이미지 슬라이드 기능을 제공하며,
         하단에 페이지네이션을 위한 점(dot)을 표시할 수 있습니다. 사용자는 이전,
@@ -385,7 +385,7 @@ export default Example;`}
         제공합니다.
       </p>
 
-      <h2 className="text-[#2D3748]">Import</h2>
+      <h2 className="text-[#2D3748]">3.1. Import</h2>
       <CodeBox
         code={`import CarouselDots from '@components/CarouselDots';`}
         copyText={`import CarouselDots from '@components/CarouselDots';`}
@@ -395,7 +395,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Usage</h2>
+      <h2 className="text-[#2D3748]">3.2. Usage</h2>
       <p>기본 사용 예제는 아래와 같습니다:</p>
       <div style={{ marginBottom: "20px" }}>
         <CarouselDots
@@ -435,7 +435,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Customization</h2>
+      <h2 className="text-[#2D3748]">3.3. Customization</h2>
       <p>
         <code>CarouselDots</code> 컴포넌트는 자동 재생, 슬라이드 간격 조정 등
         다양한 설정을 지원합니다. 필요에 따라 이러한 옵션들을 커스터마이즈할 수
@@ -498,7 +498,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Props</h2>
+      <h2 className="text-[#2D3748]">3.4. Props</h2>
       <p>
         <code>CarouselDots</code> 컴포넌트는 아래와 같은 Props를 가집니다:
       </p>
@@ -563,7 +563,7 @@ export default Example;`}
         </tbody>
       </table>
 
-      <h2 className="text-[#2D3748]">Full Example</h2>
+      <h2 className="text-[#2D3748]">3.5. Full Example</h2>
       <CodeBox
         code={`import CarouselDots from '@components/CarouselDots';
 
--- src/app/(docs)/select/page.tsx
+++ src/app/(docs)/select/page.tsx
@@ -16,14 +16,14 @@ const SelectDocs: React.FC = () => {
 
   return (
     <div className="prose max-w-[1000px] p-5 text-[#6D6D6D]">
-      <h1 className="text-[#2D3748]">Select</h1>
+      <h1 className="text-[#2D3748]">1. Select</h1>
       <p>
         <code>Select</code> 컴포넌트는 사용자가 여러 옵션 중 하나를 선택할 수
         있도록 설계된 드롭다운 메뉴입니다. 색상과 기본 선택 옵션을 조정하여
         다양한 스타일을 적용할 수 있습니다.
       </p>
 
-      <h2 className="text-[#2D3748]">Import</h2>
+      <h2 className="text-[#2D3748]">1.1. Import</h2>
       <CodeBox
         code={`import { Select } from '@components/Select';`}
         copyText={`import { Select } from '@components/Select';`}
@@ -33,7 +33,7 @@ const SelectDocs: React.FC = () => {
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Usage</h2>
+      <h2 className="text-[#2D3748]">1.2. Usage</h2>
       <p>기본 사용 예제는 아래와 같습니다:</p>
       <div style={{ marginBottom: "20px" }}>
         <Select
@@ -75,7 +75,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Changing the Color of the Select</h2>
+      <h2 className="text-[#2D3748]">1.3. Changing the Color of the Select</h2>
       <p>
         <code>Select</code> 컴포넌트는 다양한 색상으로 제공됩니다. 기본 색상은{" "}
         <code>gray</code>입니다:
@@ -180,7 +180,7 @@ function Example() {
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Props</h2>
+      <h2 className="text-[#2D3748]">1.4. Props</h2>
       <p>
         <code>Select</code> 컴포넌트는 아래와 같은 Props를 가집니다:
       </p>
@@ -233,7 +233,7 @@ function Example() {
         </tbody>
       </table>
 
-      <h2 className="text-[#2D3748]">Full Example</h2>
+      <h2 className="text-[#2D3748]">1.5. Full Example</h2>
       <CodeBox
         code={`import Select from '@components/Select';
 
@@ -267,14 +267,14 @@ function Example() {
         copied={copied}
         handleCopy={handleCopy}
       />
-      <h1 className="mb-6 mt-12 text-[#2D3748]">SelectList</h1>
+      <h1 className="mb-6 mt-12 text-[#2D3748]">2. SelectList</h1>
       <p>
         <code>SelectList</code> 컴포넌트는 여러 옵션 중 하나를 선택할 수 있는
         리스트 형태의 선택 컴포넌트입니다. 사용자는 화면에 펼쳐진 리스트에서
         옵션을 선택할 수 있습니다.
       </p>
 
-      <h2 className="text-[#2D3748]">Import</h2>
+      <h2 className="text-[#2D3748]">2.1. Import</h2>
       <CodeBox
         code={`import { SelectList } from '@components/SelectList';`}
         copyText={`import { SelectList } from '@components/SelectList';`}
@@ -284,7 +284,7 @@ function Example() {
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Usage</h2>
+      <h2 className="text-[#2D3748]">2.2. Usage</h2>
       <p>기본 사용 예제는 아래와 같습니다:</p>
       <div style={{ marginBottom: "20px" }}>
         <SelectList option={["Item 1", "Item 2", "Item 3", "Item 4"]} />
@@ -318,7 +318,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Props</h2>
+      <h2 className="text-[#2D3748]">2.3. Props</h2>
       <p>
         <code>SelectList</code> 컴포넌트는 아래와 같은 Props를 가집니다:
       </p>
@@ -359,7 +359,7 @@ export default Example;`}
         </tbody>
       </table>
 
-      <h2 className="text-[#2D3748]">Full Example</h2>
+      <h2 className="text-[#2D3748]">2.4. Full Example</h2>
       <CodeBox
         code={`import SelectList from '@components/SelectList';
 
@@ -389,7 +389,7 @@ function Example() {
         copied={copied}
         handleCopy={handleCopy}
       />
-      <h1 className="mb-6 mt-12 text-[#2D3748]">SelectValueAdd</h1>
+      <h1 className="mb-6 mt-12 text-[#2D3748]">3. SelectValueAdd</h1>
       <p>
         <code>SelectValueAdd</code> 컴포넌트는 사용자가 여러 옵션 중 여러 개를
         선택하고, 선택한 항목을 제거할 수 있는 UI 요소입니다. 이 컴포넌트는
@@ -397,7 +397,7 @@ function Example() {
         있습니다.
       </p>
 
-      <h2 className="text-[#2D3748]">Import</h2>
+      <h2 className="text-[#2D3748]">3.1. Import</h2>
       <CodeBox
         code={`import { SelectValueAdd } from '@components/SelectValueAdd';`}
         copyText={`import { SelectValueAdd } from '@components/SelectValueAdd';`}
@@ -407,7 +407,7 @@ function Example() {
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Usage</h2>
+      <h2 className="text-[#2D3748]">3.2. Usage</h2>
       <p>기본 사용 예제는 아래와 같습니다:</p>
       <div style={{ marginBottom: "20px" }}>
         <SelectValueAdd
@@ -449,7 +449,7 @@ export default Example;`}
         handleCopy={handleCopy}
       />
 
-      <h2 className="text-[#2D3748]">Props</h2>
+      <h2 className="text-[#2D3748]">3.3. Props</h2>
       <p>
         <code>SelectValueAdd</code> 컴포넌트는 아래와 같은 Props를 가집니다:
       </p>
@@ -502,7 +502,7 @@ export default Example;`}
         </tbody>
       </table>
 
-      <h2 className="text-[#2D3748]">Full Example</h2>
+      <h2 className="text-[#2D3748]">3.4. Full Example</h2>
       <CodeBox
         code={`import SelectValueAdd from '@components/SelectValueAdd';
 
