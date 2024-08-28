73c99adfb74e39041e08153f11abcfcf6150bfbf
choyoungrae <whdudfo0124@gmail.com>
2024-08-28 22:04:28 +0900

    feat : calendar docs 구현(#201)

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -92,7 +92,7 @@ const Calendar = ({ onDateSelect, defaultValue }: CalendarProps) => {
 
   return (
     <>
-      <div className="w-1/5">
+      <div className="m-auto w-2/5">
         <div className="w-full">
           <div className="mb-2 flex justify-between">
             <span>
