2adb9c62e2925051d38b1e1939088bcbb1884f74
김준수 <junsu462@naver.com>
2024-09-02 10:19:07 +0900

    save2

--- src/components/Swtich/SwitchDark.tsx
+++ src/components/Swtich/SwitchDark.tsx
@@ -7,25 +7,38 @@ const SwitchDark = () => {
   const toggleSwitch = () => {
     setIsOn(!isOn);
   };
+
   return (
     <div className="flex items-center justify-center space-x-12">
       <div
-        onClick={toggleSwitch} //
+        onClick={toggleSwitch}
         className={`flex h-7 w-14 cursor-pointer items-center rounded-full p-1 ${isOn ? `bg-[#FFFFFF]` : `bg-[#232A31]`}`}
       >
-        <div
-          className={`h-5 w-5 transform rounded-full shadow-lg duration-700 ease-in-out ${isOn ? `translate-x-7` : ``} ${isOn ? `bg-[#9AC5E5]` : `bg-[#333742]`}`}
-        />
-        <Image
-          src="/Sun.svg"
-          alt="Sun"
-          width={20}
-          height={20}
-          className="float-left"
-        />
+        {/* 이미지 컨테이너 */}
+        <div className="relative flex h-full items-center">
+          {/* 이미지 요소 */}
+          <Image
+            src="/Sun.svg"
+            alt="Sun"
+            width={20}
+            height={20}
+            className={`absolute left-1 transition-opacity duration-500 ease-in-out ${isOn ? `opacity-100` : `opacity-0`}`}
+          />
+          <Image
+            src="/Moon.svg"
+            alt="Moon"
+            width={20}
+            height={20}
+            className={`absolute translate-x-7 transition-opacity duration-500 ease-in-out ${isOn ? `opacity-0` : `opacity-100`}`}
+          />
+          {/* 스위치 버튼 */}
+          <div
+            className={`h-5 w-5 transform rounded-full shadow-lg duration-700 ease-in-out ${isOn ? `translate-x-7` : `translate-x-1`} ${isOn ? `bg-[#9AC5E5]` : `bg-[#333742]`}`}
+          />
+        </div>
       </div>
-      <br />
     </div>
   );
 };
+
 export default SwitchDark;
