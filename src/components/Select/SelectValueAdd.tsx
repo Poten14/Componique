c7a672389cb218c913c69440a6ae15afb5c2e2bd
choyoungrae <whdudfo0124@gmail.com>
2024-09-04 13:51:38 +0900

    feat : darkmode css수정(#287)

--- src/components/Select/SelectValueAdd.tsx
+++ src/components/Select/SelectValueAdd.tsx
@@ -48,7 +48,7 @@ const SelectValueAdd = ({
   return (
     <div className="relative">
       <div
-        className="flex h-9 min-w-60 max-w-[450px] cursor-pointer items-center justify-between rounded-lg border border-gray p-2 shadow-lg dark:border-[#2A6490]"
+        className="flex h-9 min-w-60 max-w-[450px] cursor-pointer items-center justify-between rounded-lg border border-gray p-2 shadow-lg dark:border-[#2A6490] dark:bg-[#2a2e39]"
         onClick={handleDropDown}
         style={{ width: open ? "auto" : "fit-content" }}
       >
@@ -57,7 +57,7 @@ const SelectValueAdd = ({
             selectValue.map((item, key) => (
               <div
                 key={key}
-                className="mr-1.5 flex items-center rounded-md bg-gray p-[2px] px-1.5 text-sm"
+                className="mr-1.5 flex items-center rounded-md bg-gray p-[2px] px-1.5 text-sm dark:bg-[#2A6490] dark:text-white"
               >
                 {item}
                 <span
@@ -72,7 +72,7 @@ const SelectValueAdd = ({
               </div>
             ))
           ) : (
-            <span className="rounded-md bg-gray p-[2px] px-1.5 text-sm">
+            <span className="rounded-md bg-gray p-[2px] px-1.5 text-sm dark:bg-[#2A6490] dark:text-white">
               {placeholder}
             </span>
           )}
@@ -83,11 +83,13 @@ const SelectValueAdd = ({
           <Image src="selectup.svg" alt="arrow" width={15} height={15} />
         )}
         {open && (
-          <ul className="absolute left-0 right-0 top-full z-30 min-w-60 max-w-[450px] rounded-lg border border-gray bg-white text-xs shadow-lg">
+          <ul className="absolute left-0 right-0 top-full z-30 min-w-60 max-w-[450px] rounded-lg border border-gray bg-white text-xs shadow-lg dark:border-[#2A6490] dark:bg-[#2a2e39]">
             {option.map((item, key) => (
               <li
-                className={`block w-full cursor-pointer rounded-lg py-2 pl-4 text-sm hover:bg-[#E8F5FF] ${
-                  selectValue.includes(item) ? "bg-[#E8F5FF]" : ""
+                className={`block w-full cursor-pointer rounded-lg py-2 pl-4 text-sm hover:bg-[#E8F5FF] dark:hover:bg-[#2A6490] dark:hover:text-white ${
+                  selectValue.includes(item)
+                    ? "bg-[#E8F5FF] dark:bg-[#2a6490] dark:text-white"
+                    : ""
                 }`}
                 key={key}
                 onClick={() => handleOptionClick(item)}
