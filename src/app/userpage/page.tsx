481126d7b6a976b1beddaddce3cdae011b3b6550
alloen9425 <allone9425@gmail.com>
2024-09-05 15:30:39 +0900

    chore: 경로 수정 (#315)

--- src/app/userpage/page.tsx
+++ src/app/userpage/page.tsx
@@ -5,40 +5,41 @@ const UserPage = () => {
   const router = useRouter();
   const templateList = [
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "SNS",
+      path: "/sns",
     },
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "Profile",
+      path: "/profile",
     },
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "Survey",
+      path: "/survey",
     },
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "ColorPicker",
+      path: "/colorpicker",
     },
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "Main",
+      path: "/main",
     },
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "Login",
+      path: "/login",
     },
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "Sign Up",
+      path: "/signup",
     },
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "Booking",
+      path: "/booking",
     },
+
     {
-      title: "webtoon",
-      path: "/webtoon",
+      title: "TodoList",
+      path: "/todolist",
     },
   ];
 
@@ -50,7 +51,7 @@ const UserPage = () => {
             key={index}
             className="relative mb-6 h-[460px] w-[300px] cursor-pointer rounded-lg bg-red-100"
             onClick={() => {
-              router.push(`/userpage/${item.path}`);
+              router.push(`/userpage${item.path}`);
             }}
           >
             <p className="absolute bottom-0 w-full rounded-b-lg bg-Basic bg-opacity-70 p-2 pb-4 text-xl">
