fb58577d8eec76d4994899406a8d91e9c0a193a5
alloen9425 <allone9425@gmail.com>
2024-09-09 10:44:27 +0900

    chore: 잃어버린 SelectList 복구 (#352)

--- src/app/userpage/(template)/profile/components/Profile.tsx
+++ src/app/userpage/(template)/profile/components/Profile.tsx
@@ -2,6 +2,7 @@
 import Icon from "@components/Icon/Icon";
 import ImageUpload from "@components/ImageUpload/Imageupload";
 import Input from "@components/Input/Input";
+import SelectList from "@components/Select/SelectList";
 import { useProfileStore } from "app/store/useProfileStore";
 const Profile = () => {
   const {
@@ -33,7 +34,10 @@ const Profile = () => {
         iconColor={iconColor}
         iconPosition={iconPosition}
       />
-
+      <SelectList
+        option={["65 Post", "65 Follower", "65 Follow "]}
+        className="rounded-lg bg-Basic text-white"
+      />
       <p className="text-md w-[120px] rounded-full bg-[#eee] p-1 text-center font-bold">
         {nickname}
       </p>
