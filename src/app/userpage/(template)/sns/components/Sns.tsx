6777c2b7e1bc8639edc9d75ef1f8111e5a6668b2
choyoungrae <whdudfo0124@gmail.com>
2024-09-11 00:02:57 +0900

    feat : 예제 추가(#389)

--- src/app/userpage/(template)/sns/components/Sns.tsx
+++ src/app/userpage/(template)/sns/components/Sns.tsx
@@ -4,6 +4,7 @@ import ImageUpload from "@components/ImageUpload/Imageupload";
 import CarouselDots from "@components/Carousel/CarouselDots";
 import Icon from "@components/Icon/Icon";
 import { useSnsStore } from "app/store/useSnsStore";
+import AvatarBasic from "@components/Avatar/AvatarBasic";
 
 const Sns = () => {
   const {
@@ -20,10 +21,10 @@ const Sns = () => {
 
   return (
     <>
-      <div className="m-auto w-[35%] rounded-lg border border-gray shadow-md">
+      <div className="m-auto w-[35%] rounded-lg border border-gray bg-white pb-5 shadow-md">
         <div className="flex w-full items-center p-2">
           <ImageUpload size={size} icon="icon-user" />
-          <span className="ml-2 font-bold dark:text-white">{nickname}</span>
+          <span className="ml-2 font-bold">{nickname}</span>
         </div>
         <CarouselDots
           images={[imageSrc1, imageSrc2, imageSrc3]}
@@ -31,42 +32,69 @@ const Sns = () => {
           showDots={showDots}
           interval={interval}
         />
-        <div className="my-2 flex w-full px-4">
+        <div className="mb-2 mt-4 flex w-full px-2">
           <Icon name="icon-heart" />
           <Icon name="icon-search" />
           <Icon name="icon-share" />
         </div>
-        <div className="w-full px-4 py-1 dark:text-white">
+        <div className="w-full px-3 py-1">
           <div>
             <span className="mr-1 font-bold">{nickname}</span>
             {content}
           </div>
         </div>
       </div>
-      <div className="m-auto mt-5 w-[35%] rounded-lg border border-gray shadow-md">
+      <div className="m-auto mt-5 w-[35%] rounded-lg border border-gray bg-white pb-5 shadow-md">
         <div className="flex w-full items-center p-2">
-          <ImageUpload size="small" icon="icon-user" />
-          <span className="ml-2 font-bold dark:text-white">Componique2</span>
+          <AvatarBasic src="/images/boomco.jpg" size={60} />
+          <span className="ml-2 font-bold">Boomco Labs</span>
         </div>
         <CarouselDots
           images={[
-            "/images/IfSc5.svg",
-            "/images/IfSc1.svg",
-            "/images/IfSc3.svg",
+            "/images/boomco3.jpg",
+            "/images/boomco5.jpg",
+            "/images/boomco4.jpg",
           ]}
           autoplay={false}
           showDots={true}
           interval={3000}
         />
-        <div className="my-2 flex w-full px-4">
+        <div className="mb-2 mt-4 flex w-full px-4">
           <Icon name="icon-heart" />
           <Icon name="icon-search" />
           <Icon name="icon-share" />
         </div>
-        <div className="w-full px-4 py-1 dark:text-white">
+        <div className="w-full px-4 py-1">
           <div>
-            <span className="mr-1 font-bold">Componique2</span>
-            일본 여행중
+            <span className="mr-1 font-bold">Boomco Labs</span>
+            붐코 커뮤니케이션
+          </div>
+        </div>
+      </div>
+      <div className="m-auto mt-5 w-[35%] rounded-lg border border-gray bg-white pb-5 shadow-md">
+        <div className="flex w-full items-center p-2">
+          <AvatarBasic src="/images/sniper.png" size={60} />
+          <span className="ml-2 font-bold">Sniperfactory</span>
+        </div>
+        <CarouselDots
+          images={[
+            "/images/sniper.png",
+            "/images/sniper2.png",
+            "/images/sniper3.jpg",
+          ]}
+          autoplay={false}
+          showDots={true}
+          interval={3000}
+        />
+        <div className="mb-2 mt-4 flex w-full px-4">
+          <Icon name="icon-heart" />
+          <Icon name="icon-search" />
+          <Icon name="icon-share" />
+        </div>
+        <div className="w-full px-4 py-1">
+          <div>
+            <span className="mr-1 font-bold">Sniperfactory</span>
+            Next.js 3기 모집
           </div>
         </div>
       </div>
