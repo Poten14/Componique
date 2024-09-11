f724f3c8343c2bef4ce2aa5ffb3e600b143ccba3
choyoungrae <whdudfo0124@gmail.com>
2024-09-11 11:39:34 +0900

    feat: todolist 이름변경(#406)

--- src/app/userpage/(template)/todolist/page.tsx
+++ src/app/userpage/(template)/todolist/page.tsx
@@ -7,7 +7,7 @@ const LoginPage: React.FC = () => {
       <div className="w-[calc(100%-350px)] p-6 pb-24 pl-[80px]">
         {/* pb-24는 로그인폼 기준 로그인폼의 아래 여백을 위해 표시한 것이므로 그외 작업시 생략 혹은 수정해도 됨 */}
         <h1 className="mb-10 ml-[70px] pb-4 text-2xl font-bold">
-          프로필 페이지
+          TodoList 페이지
         </h1>
         <TodoListForm />
       </div>
