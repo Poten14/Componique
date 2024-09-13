f7ac5c4c21cd9d27ae02658ec796e9f01a021440
김준수 <junsu462@naver.com>
2024-09-13 17:33:37 +0900

    feat: Badge Clear

--- src/app/(page)/js2/page.tsx
+++ src/app/(page)/js2/page.tsx
@@ -1,6 +1,10 @@
 import Tooltip from "@components/Tooltip/Tooltip";
 import TooltipBasic from "@components/Tooltip/TooltipBasic";
 import TooltipIcon from "@components/Tooltip/TooltipIcon";
+import BadgeBasic from "@components/Badge/BadgeBasic";
+import BadgeIcon from "@components/Badge/BadgeIcon";
+import BadgeCounter from "@components/Badge/BadgeCounter";
+import BadgeDot from "@components/Badge/BadgeDot";
 const page = () => {
   return (
     <div className="mt-40 ml-16">
@@ -10,6 +14,15 @@ const page = () => {
       <br />
       <h1>아이콘 Tooltip입니다.</h1>
       <TooltipIcon/>
+      <hr />
+      <h1>기본 Badge입니다/</h1>
+      <BadgeBasic />
+      <h1>Icon Badge입니다/</h1>
+      <BadgeIcon />
+      <h1>Counter Badge입니다/</h1>
+      <BadgeCounter />
+      <h1>Dot Badge입니다/</h1>
+      <BadgeDot />
     </div>
   );
 };
