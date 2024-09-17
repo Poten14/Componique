e8eb264e3200ab9cbcae512305892e78c1e66a17
김준수 <junsu462@naver.com>
2024-09-17 21:09:18 +0900

    clear

--- src/app/(page)/js2/page.tsx
+++ src/app/(page)/js2/page.tsx
@@ -13,6 +13,7 @@ import ValidationID from "@components/Validation/ValidationID";
 import ValidationPW from "@components/Validation/ValidationPW";
 import ValidationPhone from "@components/Validation/ValidationPhone";
 import ValidationPWConfirm from "@components/Validation/ValidationPWConfirm";
+import DataTableBasic from "@components/DataTable/DataTableBasic";
 const page = () => {
   return (
     <div className="ml-16 mt-40">
@@ -53,6 +54,45 @@ const page = () => {
       <br />
       <h1>Phone Validation입니다.</h1>
       <ValidationPhone />
+      <hr />
+      <br />
+      <h1>Basic DataTable입니다.</h1>
+      <DataTableBasic
+        data={[
+          {
+            카페이름: "달빛 정원",
+            지역: "서울 강남",
+            인기메뉴: "라벤더 라떼",
+            가격: "7,000원",
+          },
+          {
+            카페이름: "숲속의 시간",
+            지역: "경기 가평",
+            인기메뉴: "말차 스무디",
+            가격: "6,500원",
+          },
+          {
+            카페이름: "바다 위의 향기",
+            지역: "부산 해운대",
+            인기메뉴: "코코넛 아이스 커피",
+            가격: "8,000원",
+          },
+          {
+            카페이름: "구름다방",
+            지역: "제주 애월",
+            인기메뉴: "한라봉 에이드",
+            가격: "9,000원",
+          },
+          {
+            카페이름: "모던 앤 빈티지",
+            지역: "서울 마포",
+            인기메뉴: "에스프레소 토닉",
+            가격: "6,000원",
+          },
+        ]}
+        thColor="Blue"
+        tdColor="Success"
+      />
     </div>
   );
 };
