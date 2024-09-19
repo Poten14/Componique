e6ff2dcaa957d5885702478a28d4b4391542f635
choyoungrae <whdudfo0124@gmail.com>
2024-09-19 16:33:16 +0900

    feat : docs 수정

--- src/app/(docs)/introduction/page.tsx
+++ src/app/(docs)/introduction/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import CodeBox from "@components/CodeBox";
-import React, { useState } from "react";
+import { useState } from "react";
 
 const IntroducePage: React.FC = () => {
   const [copied, setCopied] = useState<{ [key: number]: boolean }>({});
